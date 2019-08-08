4ea0701b8bff0e3499ac1c9cc58f5f6ea476fd3d
vadim <duninv@gmail.com>
2019-08-08 18:15:28 +0300

    Add debug for matrix_uprade_func

--- stepik_2_6.py
+++ stepik_2_6.py
@@ -29,34 +29,19 @@ def execise_4():
         Программа должна вывести матрицу того же размера, у которой каждый элемент в позиции i, j равен сумме элементов первой матрицы на позициях (i-1, j), (i+1, j), (i, j-1), (i, j+1). У крайних символов соседний элемент находится с противоположной стороны матрицы.
         В случае одной строки/столбца элемент сам себе является соседом по соответствующему направлению.
         """
-
-
-    output_matrix = []
-
     matrix = read_matrix_input()
 
-    #print(int(matrix[2][0])+int(matrix[0][0]))
-    output_matrix = matrix
     i = 0
     j = 0
 
-    # print(matrix[0][0])
+    print(matrix[0][0])
     # print(matrix[0][1])
     # print(matrix[0][2])
-
     # print(len(matrix[0]))
+    #print(matrix[0][-1])
+
 
     print_2D_matrix(upgrade_matrix(matrix))
-
-    # while(j<len(matrix)):
-    #     while (i < len(matrix[j])):
-    #         print(matrix[j][i])
-    #         # output_matrix[i, j] = int(matrix[i-1, j])+int(matrix[i+1, j])+int(matrix[i, j-1])+int(matrix[i, j+1])
-    #         i += 1
-    #     if i>=len(matrix[j]):
-    #         i = 0
-    #     j += 1
-    #print(matrix)
 
 
 def upgrade_matrix(matrix):
@@ -66,9 +51,16 @@ def upgrade_matrix(matrix):
     while (int(i) < len(matrix)):
         while (int(j) < len(matrix[i])):
             try:
+                print("i,j = "+str(i)+" "+str(j)+" matrix[i-1][j] real_coord({} {}) = ".format(str(i-1), str(j))+str(matrix[i-1][j]))
+                print("i,j = "+str(i)+" "+str(j)+" matrix[i+1][j] real_coord({} {})  = ".format(str(i+1), str(j))+str(matrix[i+1][j]))
+                print("i,j = "+str(i)+" "+str(j)+" matrix[i+1][j] real_coord({} {})  = ".format(str(i+1), str(j))+str(matrix[i+1][j]))
+                print("i,j = "+str(i)+" "+str(j)+" matrix[i][j+1] real_coord({} {})  = ".format(str(i), str(j+1))+str(matrix[i][j+1]))
+                print("\n")
+
                 output_matrix[i][j] = matrix[i-1][j] + matrix[i+1][j] + matrix[i][j-1] + matrix[i][j+1]
             except IndexError:
-                print(output_matrix[i][j]+" ошибка индекса")
+                pass
+                #print(output_matrix[i][j]+" ошибка индекса")
             j += 1
         j = 0
         i += 1
