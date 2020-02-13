4f805045b87972a3389d3548a9073169e920b5f6
Vadim Dunin <duninv@gmail.com>
2020-02-13 23:12:06 +0300

    lesson6_step_11

--- Autotests_with_Selenium_and_Python/lesson6_step_11.py
+++ Autotests_with_Selenium_and_Python/lesson6_step_11.py
@@ -1,21 +1,19 @@
+
+
 from selenium import webdriver
 import time
 from Autotests_with_Selenium_and_Python.settings import chrome_driver_bin_path
 
 try:
-    link = "http://suninjuly.github.io/registration1.html"
+    link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome(executable_path=chrome_driver_bin_path)
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
     ...
-    # required_elements = browser.find_elements_by_css_selector('input[required]')
-    # for element in required_elements:
-    #     element.send_keys("test")
-
     required_css_selectors_list = ['input[placeholder="Input your name"]',
-                                    'input[placeholder="Input your last name"]',
-                                    'input[placeholder="Input your email"]']
+                         'input[placeholder="Input your last name"]',
+                         'input[placeholder="Input your email"]']
 
     for css_selector in required_css_selectors_list:
         element = browser.find_element_by_css_selector(css_selector)
