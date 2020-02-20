c532b1d30a236d714770a245352e90d6e5b324dd
Vadim Dunin <duninv@gmail.com>
2020-02-20 09:01:20 +0300

    lesson_2.2_step_7
    upload file example

--- Autotests_with_Selenium_and_Python/lesson22_step7.py
+++ Autotests_with_Selenium_and_Python/lesson22_step7.py
@@ -1,1 +1,9 @@
-# Загрузка файлов
+import os
+
+current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла
+print("Current Directory:")
+print(current_dir)
+
+# file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла
+# element.send_keys(file_path)
+# file_path = os.path.join(current_dir, 'file.txt')
