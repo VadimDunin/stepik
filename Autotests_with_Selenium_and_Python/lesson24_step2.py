b918e057e7a5590daf92d00e0244740a33fc7500
Vadim Dunin <duninv@gmail.com>
2020-02-20 15:23:58 +0300

    lesson_2.4_step_5
    implicitly_wait

--- Autotests_with_Selenium_and_Python/lesson24_step2.py
+++ Autotests_with_Selenium_and_Python/lesson24_step2.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 import time
 
-quick_fix = True
+
+time_sleep_fix = False
+implicit_fix = True
+
+
+# browser.implicitly_wait(5) - проверка искомого жлемента будет происходить каждые 500 мс в течении 5 секунд
+
 
 browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/wait1.html")
-if quick_fix:
+if time_sleep_fix:
     time.sleep(1)
+if implicit_fix:
+    browser.implicitly_wait(5)
+
 button = browser.find_element_by_id("verify")
 button.click()
 message = browser.find_element_by_id("verify_message")
