e126efd22b321d2bb6b49118f11bb7809a21db04
Vadim Dunin <duninv@gmail.com>
2020-03-16 10:38:55 +0300

    lesson33_step6
    test_*.py, *_test.py

---
+++ Autotests_with_Selenium_and_Python/lesson33_step6/test_project/test_regression.py
@@ -0,0 +1,16 @@
+class TestLessonCreate:
+    def test_create_lesson(self, browser):
+        pass
+
+    def user_with_lesson_can_create_lesson_from_navbar_test(self, browser):
+        pass
+
+
+class CourseCreate:
+
+    def test_create_course(self, browser):
+        pass
+
+
+def test_guest_can_open_new_course(browser):
+    pass
